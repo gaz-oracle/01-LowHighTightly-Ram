c3abbff83dcc0cccb0390f28c4fff3eca67271de
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-03-31 17:02:57 -0600

    Commit 1º - @Qualifier vs @Primary - description

--- src/main/java/com/gaz/app/di/Client.java
+++ src/main/java/com/gaz/app/di/Client.java
@@ -10,7 +10,15 @@ public class Client {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MessageSender messageSender = applicationContext.getBean(MessageSender.class);
         messageSender.sendMessage(message);
+        /*
+         @Primary indica que se debe dar preferencia a un bean cuando varios candidatos están calificados
+         para autoconectar una dependencia de un solo valor.
 
+         @Qualifier indica que un bean específico debe conectarse automáticamente cuando hay varios candidatos
+
+         Por lo tanto, puede encontrar que @Qualifier y @Primary le dicen a Spring que use el bean específico
+         cuando hay varios candidatos están calificados para la conexión automática. Pero @Qualifier es más específico
+         y tiene alta prioridad. Entonces, cuando se encuentran @Qualifier y @Primary, se ignorará @Primary.
+         */
     }
-
 }
